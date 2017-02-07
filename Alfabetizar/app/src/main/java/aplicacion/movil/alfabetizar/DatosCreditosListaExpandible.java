86ad78be98dff65fc75851c5a42df94db6960d60
José Eduardo Picado Salas <jedups95@outlook.com>
2017-02-06 22:09:27 -0600

    Agregado el texto y secciones de los créditos

--- Alfabetizar/app/src/main/java/aplicacion/movil/alfabetizar/DatosCreditosListaExpandible.java
+++ Alfabetizar/app/src/main/java/aplicacion/movil/alfabetizar/DatosCreditosListaExpandible.java
@@ -14,24 +14,62 @@ public class DatosCreditosListaExpandible {
         HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
 
         List<String> seccion1 = new ArrayList<String>();
-        seccion1.add("INSERTAR TEXTO #1... " +
-                "\n" +
-                "\uD83D\uDE0A");
+        seccion1.add(
+                "Dra. María Marta Camacho Alvarez\n" +
+                "Profesora Catedrática \n" +
+                "Universidad de Costa Rica " +
+                "\n\n" +
+                "Dra. Marielos Murillo Rojas\n" +
+                "Profesora Catedrática\n" +
+                "Universidad de Costa Rica " +
+                "\n\n" +
+                "Dr. Carlos Sánchez Avendaño\n" +
+                "Profesor Catedrático\n" +
+                "Universidad de Costa Rica");
 
         List<String> seccion2 = new ArrayList<String>();
-        seccion2.add("INSERTAR TEXTO #2... " +
-                "\n" +
-                "\uD83D\uDE18");
+        seccion2.add(
+                "Señor\n" +
+                "Adrián Brenes Jiménez \n" +
+                "Técnico en Grabación\n" +
+                "Radioemisoras UCR" +
+                "\n\n" +
+                "Señora\n" +
+                "Catherine Prado Goyenaga\n" +
+                "Coordinadora General \n" +
+                "Radioemisoras UCR");
 
         List<String> seccion3 = new ArrayList<String>();
-        seccion3.add("INSERTAR TEXTO #3...  " +
-                "\n" +
-                "\uD83D\uDE33");
+        seccion3.add(
+                "Señorita\n" +
+                "Isabel Acuña Calderón\n" +
+                "Estudiante de Diseño Gráfico \n" +
+                "Universidad de Costa Rica");
+
+        List<String> seccion4 = new ArrayList<String>();
+        seccion4.add(
+                "Señorita\n" +
+                "Larissa Arroyo Castillo\n" +
+                "Estudiante de Computación e Informática \n" +
+                "Universidad de Costa Rica" +
+                "\n\n" +
+                "Señorito\n" +
+                "Luis Leandro Jiménez\n" +
+                "Estudiante de Computación e Informática \n" +
+                "Universidad de Costa Rica"  +
+                "\n\n" +
+                "Señorito\n" +
+                "José Eduardo Picado Salas\n" +
+                "Estudiante de Computación e Informática \n" +
+                "Universidad de Costa Rica");
 
 
-        expandableListDetail.put("INSERTAR TITULO #1... ", seccion1);
-        expandableListDetail.put("INSERTAR TITULO #2... ", seccion2);
-        expandableListDetail.put("INSERTAR TITULO #3... ", seccion3);
+        expandableListDetail.put("Programación", seccion4);
+        expandableListDetail.put("Diseño", seccion3);
+        expandableListDetail.put("Audio", seccion2);
+        expandableListDetail.put("Coordinación ", seccion1);
+
+
         return expandableListDetail;
     }
 }
